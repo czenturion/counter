381fafd9a03514f0e7391e40221d089d1a64a01e
czenturion <czenturijon@gmail.com>
2023-02-13 15:57:07 +0300

    v 0.1 | created functionality by hook useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './index.scss';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+
+    const countMinus = () => {
+        setCount(count - 1);
+        console.log(count)
+    };
+    const countPlus = () => {
+        setCount(count + 1);
+    };
+
+    return (
+        <div className="App">
+            <div>
+                <h2>Счетчик:</h2>
+                <h1>{count}</h1>
+                <button className="minus" onClick={countMinus}>- Минус</button>
+                <button className="plus" onClick={countPlus}>Плюс +</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
